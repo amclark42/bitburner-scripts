43956c5a7b3a2c73bdc9c9b83e169f35f607e1ac
Ash Clark <amclark42@gmail.com>
2022-05-11 09:23:04 -0400

    Add script to copy multiple files to another server

---
+++ copy-scripts-to.js
@@ -0,0 +1,26 @@
+/** @param {NS} ns */
+/*
+  Copy scripts from the current server to another.
+*/
+export async function main(ns) {
+  const recipientName = ns.args[0],
+    defaultFiles = ['library.js', 'weaken.js', 'grow-n-hack.js', 'hack-me.js', 'what-about.js'],
+    files = ns.args.slice(1).length > 0 ? ns.args.slice(1) : defaultFiles;
+  if (typeof recipientName !== 'string' || recipientName === 'help') {
+    ns.tprint("Usage: run copy-scripts-to.js [-t THREADS] SERVER-NAME [SCRIPT1 [SCRIPT2 ...]]");
+    ns.tprint("If no scripts are named, these will be copied to the target server:");
+    for (const filename of defaultFiles) {
+      ns.tprint("  • "+filename);
+    }
+    return;
+  } else if ( !ns.serverExists(recipientName) ) {
+    ns.tprint("There is no server named '"+recipientName+"'!");
+    return;
+  }
+  ns.tprint("Copying script(s) to '"+recipientName+"': "+files.join(', '));
+  if ( await ns.scp(files, recipientName) ) {
+    ns.tprint("Done!");
+  } else {
+    ns.tprint("ERROR: Something went wrong.");
+  }
+}
